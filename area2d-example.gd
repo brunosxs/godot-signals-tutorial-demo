ba82a479b1980901f8c3c26622ec4caf4036b848
Bruno SXS <brunosxs@gmail.com>
2017-02-18 16:25:04 -0200

    added more details to the comments

--- area2d-example.gd
+++ area2d-example.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
+# Don't forget to check the "Visible collision shapes" so you can see the area2D at runtime
 
-func _on_Area2D_body_enter(body): # The name you give to the argument doesn't really matter, just understand that this function will always receive an argument and if you don't provide a variable for it, then an error will occur. 
+func _on_Area2D_body_enter(body): # The name you give to the argument doesn't really matter, just understand that this function will always receive an argument and if you don't provide a variable name for it, then an error will occur. 
 	if body extends KinematicBody2D:
 		print("Someone entered our territory!") # Any kinematic body that enters this area will  make the code display this message.
 		if body.get_name() == "player":
